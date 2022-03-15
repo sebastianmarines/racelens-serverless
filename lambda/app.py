e4fe61dfa39842ac6f0e97c7a3f1ac83e9ff5a50
Sebastian Marines <sebastian0marines@gmail.com>
2022-03-14 19:05:08 -0600

    Fix HTTP error codes
    Fix HTTP error codes for invalid file type and missing query parameter.

--- lambda/app.py
+++ lambda/app.py
@@ -83,13 +83,20 @@ def generate_upload_url(event, _context):
     SUPPORTED_EXTENSIONS = ["jpg", "jpeg", "png"]
 
     event_name = event['pathParameters']['event_name']
-    file_type = event['queryStringParameters']['file_type']
     access_token = event['headers'].get('Authorization', None)
+
+    try:
+        file_type = event['queryStringParameters']['file_type']
+    except TypeError:
+        return {
+            'statusCode': 422,
+            'body': json.dumps('Missing file_type query parameter')
+        }
 
 
     if file_type not in SUPPORTED_EXTENSIONS:
         return {
-            'statusCode': 400,
+            'statusCode': 415,
             'body': json.dumps('Invalid file type')
         }
 
