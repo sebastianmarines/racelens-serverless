1701c48da52590a2bc7eb2085a930464747da9bf
Sebastian Marines <sebastian0marines@gmail.com>
2022-03-28 21:41:52 -0600

    Read updated env variables in models.py

--- lambda/models.py
+++ lambda/models.py
@@ -1,14 +1,15 @@
 import os
 
-from pynamodb.attributes import UnicodeAttribute
+from pynamodb.attributes import UnicodeAttribute, UTCDateTimeAttribute
 from pynamodb.models import Model
 
-DYNAMODB_TABLE_NAME = os.environ['DYNAMODB_TABLE_NAME']
+IMAGES_DYNAMODB_TABLE_NAME = os.environ['IMAGES_DYNAMODB_TABLE_NAME']
+EVENTS_DYNAMODB_TABLE_NAME = os.environ['EVENTS_DYNAMODB_TABLE_NAME']
 
 
 class ImageModel(Model):
     class Meta:
-        table_name = DYNAMODB_TABLE_NAME
+        table_name = IMAGES_DYNAMODB_TABLE_NAME
     
     event_id = UnicodeAttribute(hash_key=True)
     image_id = UnicodeAttribute(range_key=True)
