082663fc0e465a6a6fe7c58dd4fa7594dd9e3dfc
Sebastian Marines <sebastian0marines@gmail.com>
2022-03-28 20:27:04 -0600

    Create Image model

--- lambda/models.py
+++ lambda/models.py
@@ -6,9 +6,11 @@ from pynamodb.models import Model
 DYNAMODB_TABLE_NAME = os.environ['DYNAMODB_TABLE_NAME']
 
 
-class Event(Model):
+class ImageModel(Model):
     class Meta:
         table_name = DYNAMODB_TABLE_NAME
     
-    pk = UnicodeAttribute(hash_key=True)
-    sk = UnicodeAttribute(range_key=True)
+    event_id = UnicodeAttribute(hash_key=True)
+    image_id = UnicodeAttribute(range_key=True)
+
+    s3_object = UnicodeAttribute()
